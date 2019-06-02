8b10946d3817d0f471185f704d0e702ea59cc202
Matías Martínez <matias.martinez@rappi.com>
2019-06-02 18:07:34 -0300

    Make Forms look nicer.

--- proyecto/servicios/forms.py
+++ proyecto/servicios/forms.py
@@ -3,50 +3,60 @@ from django import forms
 from .models import Servicio, Tarea, ParteDeTrabajo, Cliente, Cuadrilla, EmpleadoDeCuadrilla
 
 
-class ServicioForm(forms.ModelForm):
+class NicerForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(NicerForm, self).__init__(*args, **kwargs)
+        for field_name in self.fields:
+            field = self.fields.get(field_name)
+            if field:
+                field.widget.attrs.update({
+                    'class': "form-control w-25"
+                })
+
+
+class ServicioForm(NicerForm):
 
     class Meta:
         model = Servicio
         fields = ('descripcion', 'cliente', 'fecha_solcitud', 'fecha_inicio', 'fecha_fin', 'estado', 'fecha_fin',)
         
 
-class TareaForm(forms.ModelForm):
+class TareaForm(NicerForm):
 
     class Meta:
         model = Tarea
         fields = ('descripcion', 'servicio', 'estado',)
         
 
-class ParteForm(forms.ModelForm):
+class ParteForm(NicerForm):
 
     class Meta:
         model = ParteDeTrabajo
         fields = ('descripcion', 'tarea', 'fecha', 'aprobado_por')
 
-class ParteJefeForm(forms.ModelForm):
+class ParteJefeForm(NicerForm):
 
     class Meta:
         model = ParteDeTrabajo
         fields = ('descripcion', 'tarea', 'fecha')
         
 
-class ClienteForm(forms.ModelForm):
+class ClienteForm(NicerForm):
 
     class Meta:
         model = Cliente
         fields = ('nombre', 'direccion', 'descripcion', 'telefono')
         
         
-class CuadrillaForm(forms.ModelForm):
+class CuadrillaForm(NicerForm):
 
     class Meta:
         model = Cuadrilla
         fields = ('especialidad', 'vehiculo')
         
         
-class EmpleadoForm(forms.ModelForm):
+class EmpleadoForm(NicerForm):
 
     class Meta:
         model = EmpleadoDeCuadrilla
         fields = ('legajo', 'cuil', 'cuadrilla', 'nombre', 'apellido', 'empleado_desde', 'fecha_nac', 'es_jefe')
-
